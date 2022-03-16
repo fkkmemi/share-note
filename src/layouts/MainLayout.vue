abb559ab44c5dce093fdbae1fa8afe1a59980db9
fkkmemi <fkkmemi@gmail.com>
2022-03-16 15:44:10 +0900

    auth test

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,3 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { auth } from 'src/boot/firebase'
+import {
+  GoogleAuthProvider, signInWithPopup, signOut,
+  onAuthStateChanged
+} from 'firebase/auth'
+const drawer = ref(false)
+const provider = new GoogleAuthProvider()
+const logIn = () => {
+  signInWithPopup(auth, provider)
+}
+
+const logOut = () => {
+  signOut(auth)
+}
+
+onAuthStateChanged(auth, (user) => {
+  console.log('log')
+  console.log(user)
+})
+</script>
+
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
@@ -8,19 +31,20 @@
           round
           icon="menu"
           aria-label="Menu"
-          @click="toggleLeftDrawer"
+          @click="drawer = !drawer"
         />
 
         <q-toolbar-title>
           Quasar App
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn icon="mdi-login" round flat @click="logIn" />
+        <q-btn icon="mdi-logout" round flat @click="logOut" />
       </q-toolbar>
     </q-header>
 
     <q-drawer
-      v-model="leftDrawerOpen"
+      v-model="drawer"
       show-if-above
       bordered
     >
@@ -30,12 +54,6 @@
         >
           Essential Links
         </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
       </q-list>
     </q-drawer>
 
@@ -44,73 +62,3 @@
     </q-page-container>
   </q-layout>
 </template>
-
-<script lang="ts">
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
-
-export default defineComponent({
-  name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
-
-  setup () {
-    const leftDrawerOpen = ref(false)
-
-    return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
-</script>
